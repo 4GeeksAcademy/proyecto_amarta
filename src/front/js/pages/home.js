cc0be61c638a385a2e4757e6136984e80053924e
bertablancpastor <72865106+bertablancpastor@users.noreply.github.com>
2023-08-16 11:53:21 +0000

    build home images triangle shop

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,56 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import homeImageUrl from "../../img/img293.jpg";
+import img1 from "../../img/1.png"
+import img2 from "../../img/2.png"
+import img3 from "../../img/3.png"
+import img4 from "../../img/4.png"
+import img5 from "../../img/5.png"
+import img6 from "../../img/6.png"
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+	// const { store, actions } = useContext(Context);
+
+	const images = [img1, img2, img3, img4, img5, img6]
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div className="text-center">
+			<div id="" className="">
+
+				<div className="">
+					<img src={homeImageUrl} alt="..." className="img-fluid"  />
+					<div className="">
+						<h1 className="position-absolute top-50 start-50 translate-middle text-white title-home"> Belleza
+							simplificada.
+							Solo el
+							mejor
+							producto natural
+							en
+							cada categoria
+							de belleza. Todo lo que
+							necessitas y nada que no.
+						</h1>
+				
+						<Link to={"#"} type="button" className="translate-middle btn-shop">Shop</Link>
+					</div>
+				</div>
+
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+
+			
+
+			{/* //MAPEO DE IMAGENES */}
+			<div className="container-fluid">
+				<div className="row img-fluid p-0 ">
+					<div className="col-sm-12 col-md-2 col-lg-2 m-2" style={{ width: "100%", height: "100%" }}>
+						{images.map(item => (item))}
+					</div>
+					
+				</div>
+			</div>
+
 		</div>
 	);
 };
