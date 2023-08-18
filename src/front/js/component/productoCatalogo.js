c9cb141c6de1708801f0266376d1b97f9fce3187
bertablancpastor <72865106+bertablancpastor@users.noreply.github.com>
2023-08-18 13:56:10 +0000

    feaat style ul && li tipo producto

--- src/front/js/component/productoCatalogo.js
+++ src/front/js/component/productoCatalogo.js
@@ -3,19 +3,21 @@ import PropTypes from "prop-types";
 import "../../styles/producto-catalogo.css";
 
 export const ProductoCatalogo = props => {
-  
+
     return (
-           
-            <div className="card col-12 col-md-6 col-lg-4 mx-3 border-0 pb-4">
-                <img src={props.producto.imagen} className="card-img-top  rounded-0" alt="..." />
-                <div className="card-body container">
-                    <h5 className="card-title d-flex">{props.producto.nombre}</h5>
-                    <p className="text-end fw-lighter">{props.producto.precio} €</p>
-                    <p className="card-text">{props.producto.ingredientes_prin}</p>
-                    <p className="card-text">{props.producto.propiedades}</p>
-                    <a href="#" className="btn btn-outline-secondary btn-sm rounded-0">Mas información</a>
+
+        <div className="card col-12 col-md-6 col-lg-4 mx-3 border-0 pb-4">
+            <img src={props.producto.imagen} className="card-img-top  rounded-0" alt="..." />
+            <div className="card-body container">
+                <div className="d-flex ">
+                    <h5 className="card-title me-5 ">{props.producto.nombre}</h5>
+                    <p className="text-end fw-lighter text-end">{props.producto.precio} €</p>
                 </div>
+                <p className="card-text">{props.producto.ingredientes_prin}</p>
+                <p className="card-text">{props.producto.propiedades}</p>
+                <a href="#" className="btn btn-outline-secondary btn-sm rounded-0">Mas información</a>
             </div>
+        </div>
 
     );
 };
--- src/front/js/pages/catalogo.js
+++ src/front/js/pages/catalogo.js
@@ -7,35 +7,35 @@ import { ProductoCatalogo } from "../component/productoCatalogo";
 
 export const Catalogo = () => {
 	const { store, actions } = useContext(Context);
-	
+
 	const [filter, setFilter] = useState("")
 
 
 	useEffect(() => {
 		actions.getTipoProducto()
 		actions.getProducts()
-		
+
 	}, [])
 
 	return (
 		<div className="">
 			<div className="container-fluid bg-secondary-subtle p-5">
-				<h4 className="title-catalogo">Belleza simplificadad. Solo con el mejor producto natural en cada categoria de belleza. Todo
-					lo que necessitas y nada que no.
-				</h4>
-				<ul className="d-inline p-2">
-					<li onClick={e=> setFilter("")}>Todos</li>
-					{store.tipo_producto?.map(item => <li key={item.id_tipo} id={item.id_tipo} onClick={e => setFilter(item.id_tipo)}>{item.nombre}</li>)}</ul>
+				<h4 className="title-catalogo pb-2">Belleza simplificadad.</h4>
+				<h4 className="title-catalogo pb-2">Solo con el mejor producto natural en cada categoria de belleza.</h4>
+				<h4 className="title-catalogo pb-2">Todo lo que necessitas y nada que no.</h4>
+				<ul className="list-group list-group-horizontal p-2">
+					<li className="list-group-item" onClick={e => setFilter("")}>Todos</li>
+					{store.tipo_producto?.map(item => <li className="list-group-item" key={item.id_tipo} id={item.id_tipo} onClick={e => setFilter(item.id_tipo)}>{item.nombre}</li>)}</ul>
 			</div>
 
 			<div className="d-flex ">
-                {store.productos?.filter(item => filter === "" ? store.productos : item.id_tipo === filter).map(item => (
-                    <ProductoCatalogo
-                        key={item.id_producto}
-						producto = {item}
-                    />
-                ))}
-            </div>
+				{store.productos?.filter(item => filter === "" ? store.productos : item.id_tipo === filter).map(item => (
+					<ProductoCatalogo
+						key={item.id_producto}
+						producto={item}
+					/>
+				))}
+			</div>
 
 		</div>
 	);
