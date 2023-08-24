6a991c2e8957248d17a6b48f7ddf17774aa3c9de
bertablancpastor <72865106+bertablancpastor@users.noreply.github.com>
2023-08-24 09:51:13 +0000

    build email newsletter form consulta messaje send

--- src/front/js/pages/contacto.js
+++ src/front/js/pages/contacto.js
@@ -14,16 +14,35 @@ export const Contacto = () => {
 	// 	actions.getProducts()
 
 	// }, [])
+	const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+	const appendAlert = (message, type) => {
+		const wrapper = document.createElement('div')
+		wrapper.innerHTML = [
+			`<div class="alert alert-${type} alert-dismissible mt-3" role="alert">`,
+			`   <div>${message}</div>`,
+			'   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+			'</div>'
+		].join('')
+
+		alertPlaceholder.append(wrapper)
+	}
+
+	const alertTrigger = document.getElementById('liveAlertBtn')
+	if (alertTrigger) {
+		alertTrigger.addEventListener('click', () => {
+			appendAlert('Mensaje enviado!', 'dark')
+		})
+	}
 
 	return (
 		<div className="min-vh-100">
 			<div className="container-fluid bg-secondary-subtle p-5 bg-white bg-opacity-50">
-				<div className="container-fluid row">
+				<div className="container-fluid row mb-5">
 					<div className="col">
 						<p className="h1 pb-5">¡En que te podemos ayudar!</p>
 						<p>La belleza de tu historia es que continuará evolucionando y tu sitio evolucionará con ella.</p>
-						<p>correoelectronico@ejemplo.com</p>
-						<p>555-5555</p>
+						<p className="h5 pb-2">correoelectronico@ejemplo.com</p>
+						<p className="h5 ">555-5555</p>
 					</div>
 					<div className="col">
 						<div className="row">
@@ -46,8 +65,25 @@ export const Contacto = () => {
 							<label for="textArea" className="form-label">Mensaje</label>
 							<textarea className="form-control" id="textArea" rows="3"></textarea>
 						</div>
-						<button type="button" className="btn btn-dark btn-lg px-4 me-md-2">Enviar</button>
+						<div id="liveAlertPlaceholder">
+							<button type="button" className="btn btn-dark me-md-2" id="liveAlertBtn">Enviar</button>
+						</div>
+
 					</div>
+				</div>
+				<div className="row g-3 mt-5">
+					<div className="col">
+						<p className="h3">¡Especialmente para ti!</p>
+						<p className="fs-6 mb-0">Regístrate y sé la primera persona en enterarte de los descuentos!</p>
+						<p className="fs-6 ">Recibe los consejos y todo lo que ofrece AMARTA.</p>
+					</div>
+					<div className="col">
+						<input type="password" className="form-control mb-3" placeholder="Correo electronico" />
+						<button type="submit" className="btn btn-dark  me-md-2">Subscribirse</button>
+					</div>
+
+
+
 				</div>
 			</div>
 		</div>
