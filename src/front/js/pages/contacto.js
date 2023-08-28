2e5f0b213990b32a2c3e2e273f3472d995fef20c
bertablancpastor <72865106+bertablancpastor@users.noreply.github.com>
2023-08-28 08:16:52 +0000

    build pagina contacto

--- src/front/js/pages/contacto.js
+++ src/front/js/pages/contacto.js
@@ -4,7 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const Contacto = () => {
-	const {  actions } = useContext(Context);
+	const { actions } = useContext(Context);
 
 	// const [filter, setFilter] = useState("")
 
@@ -26,7 +26,7 @@ export const Contacto = () => {
 		actions.enviarMensaje(nombre, apellido, email, mensaje)
 	}
 
-
+	//Alert de los boton de enviar formulario
 	const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 	const appendAlert = (message, type) => {
 		const wrapper = document.createElement('div')
@@ -47,19 +47,44 @@ export const Contacto = () => {
 		})
 	}
 
+	//Alert de los boton de subscribirse a la NL
+	const alertPlaceholderNL = document.getElementById('liveAlertPlaceholderNL')
+
+	const appendAlertNL = (message, type) => {
+		const wrapper = document.createElement('div')
+		wrapper.innerHTML = [
+			`<div class="alert alert-${type} alert-dismissible mt-3" role="alert">`,
+			`   <div>${message}</div>`,
+			'   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+			'</div>'
+		].join('')
+
+		alertPlaceholderNL.append(wrapper)
+	}
+
+	const alertTiggerNL = document.getElementById('livealertBtnNL')
+	if(alertTiggerNL){
+		alertTiggerNL.addEventListener('click', () => {
+			appendAlertNL('Subscrito con éxito!', 'dark')
+	
+		})
+	}
+
+
+
 	return (
 		<div className="min-vh-100">
 			<div className="container-fluid bg-secondary-subtle p-5 bg-white bg-opacity-50">
 				<div className="container-fluid row mb-5">
 					<div className="col">
-						<p className="h1 pb-5">¡En que te podemos ayudar!</p>
-						<p>La belleza de tu historia es que continuará evolucionando y tu sitio evolucionará con ella.</p>
+						<p className="h1 pb-5">¡En qué te podemos ayudar!</p>
+						<p className="fw-bold">La belleza de tu historia es que continuará evolucionando y tu sitio evolucionará con ella.</p>
 						<p className="h5 pb-2">correoelectronico@ejemplo.com</p>
 						<p className="h5 ">555-5555</p>
 					</div>
 					<div className="col">
 						<div className="row">
-							<div className="col">
+							<div className="col pb-2">
 								<label for="nombre" className="form-label">Nombre</label>
 								<input type="text" className="form-control" placeholder="Nombre" id="nombre" aria-label="Nombre" onChange={e => setNombre(e.target.value)} />
 							</div>
@@ -69,14 +94,14 @@ export const Contacto = () => {
 							</div>
 						</div>
 						<div className="row">
-							<div className="col">
+							<div className="col pb-2">
 								<label for="email" className="form-label">Email</label>
-								<input type="text" className="form-control" id="email" placeholder="Email" aria-label="Email" onChange={e => setEmail(e.target.value)}  />
+								<input type="text" className="form-control" id="email" placeholder="Email" aria-label="Email" onChange={e => setEmail(e.target.value)} />
 							</div>
 						</div>
 						<div className="mb-3">
 							<label for="textArea" className="form-label">Mensaje</label>
-							<textarea className="form-control" id="textArea" rows="3" onChange={e => setMensaje(e.target.value)} ></textarea>
+							<textarea className="form-control" id="textArea" rows="3" placeholder="¿En qué te podemos ayudar?" onChange={e => setMensaje(e.target.value)} ></textarea>
 						</div>
 						<div id="liveAlertPlaceholder">
 							<button type="button" className="btn btn-dark me-md-2" id="liveAlertBtn" onClick={e => handleEnviarMensaje(e)}>Enviar</button>
@@ -84,15 +109,15 @@ export const Contacto = () => {
 
 					</div>
 				</div>
-				<div className="row g-3 mt-5">
+				<div className="container-fluid row ">
 					<div className="col">
 						<p className="h3">¡Especialmente para ti!</p>
-						<p className="fs-6 mb-0">Regístrate y sé la primera persona en enterarte de los descuentos!</p>
-						<p className="fs-6 ">Recibe los consejos y todo lo que ofrece AMARTA.</p>
+						<p className="fs-6 mb-0 fw-bold">Regístrate y sé la primera persona en enterarte de los descuentos!</p>
+						<p className="fs-6 fw-bold">Recibe los consejos y todo lo que ofrece AMARTA.</p>
 					</div>
-					<div className="col">
+					<div className="col" id="liveAlertPlaceholderNL">
 						<input type="password" className="form-control mb-3" placeholder="Correo electronico" />
-						<button type="submit" className="btn btn-dark  me-md-2">Subscribirse</button>
+						<button type="submit" className="btn btn-dark  me-md-2" id="livealertBtnNL">Subscribirse</button>
 					</div>
 
 
