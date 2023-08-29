ba21e03d6656e3c80889663d61fa5a3e84664670
Laura Espiño <126553407+LauraEspino@users.noreply.github.com>
2023-08-29 19:12:42 +0000

    perfil avanzado, ver pedidos luego

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { ProductoCatalogo } from "../component/productoCatalogo";
 
 export const Private = () => {
@@ -31,13 +31,23 @@ export const Private = () => {
     if (status === "authorized") {
         return (
             <div className=" min-vh-100">
-                <h1>Loggeado como {store.user.email}</h1>
-                <div className="d-flex" id="arrayFavoritos">
-                    {store.favs.map(item => (
-                        <ProductoCatalogo key={item.id_producto} producto={item}></ProductoCatalogo>
-                    ))}
+			    <div className="container-fluid bg-secondary-subtle p-5 bg-white bg-opacity-50">
+                    <h1>Mi perfil</h1>
+                    <hr></hr>
+                    <h3 className="d-flex justify-content-center mt-2 mb-2"><h3>Correo de registro: </h3> <h3 className="fst-italic fw-bolder ms-1"> {store.user.email}</h3></h3>
+                    <div  id="arrayFavoritos">
+                    <h4 className="d-block">Tus favoritos: ({store.favs.length}) </h4>
+                        {store.favs.length === 0 ? (
+                            <h5  className="d-block ms-5 mt-2 mb-2">Aún no tienes favoritos.
+                            
+                            <Link to={"/catalogo"} type="button" className="d-block w-25 btn btn btn-dark btn-lg px-4 mt-3">Ir al catálogo</Link></h5>
+                        ) : (
+                            store.favs.map(item => (
+                                <ProductoCatalogo key={item.id_producto} producto={item}></ProductoCatalogo>
+                            ))
+                        )}
+                    </div>
                 </div>
-                <button onClick={logFavs}>favs</button>
             </div>
         )
     }
@@ -50,9 +60,12 @@ export const Private = () => {
 
     return (
         <>
-            <div className="text-center min-vh-100">
-                <h1>Checking validation...</h1>
+            <div className="mt-3 text-center min-vh-100">
+                <h1>Verificando su cuenta...</h1>
+                <div class="spinner-border text-dark" role="status">
+                </div>
             </div>
+            
         </>
     )
 
