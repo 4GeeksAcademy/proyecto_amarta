70b4ee204950f4764b42d35461800f5555283a7f
Laura Espiño <126553407+LauraEspino@users.noreply.github.com>
2023-09-13 11:15:25 +0000

    se agrega buscador en catalogo + modificaciones en diseño del div de encabezado

--- src/front/js/pages/catalogo.js
+++ src/front/js/pages/catalogo.js
@@ -9,36 +9,76 @@ import "../../styles/catalogo.css"
 export const Catalogo = () => {
 	const { store, actions } = useContext(Context);
 
-	const [filter, setFilter] = useState("")
-
-
+	const [filter, setFilter] = useState("");
+	const [busquedaProducto, setbusquedaProducto] = useState("");
+	const [busquedaResultado, setbusquedaResultado] = useState([]);
+  
 	useEffect(() => {
-		actions.getTipoProducto()
-		actions.getProducts()
-
-	}, [])
+	  actions.getTipoProducto();
+	  actions.getProducts();
+	}, []);
+  
+	// Filtrar los productos por tipo si hay un filtro seleccionado
+	const filtroProductos =
+	  filter === ""
+		? store.productos
+		: store.productos.filter((item) => item.id_tipo === filter);
+  
+	// Filtrar los productos por término de búsqueda
+	const filtroPorBusqueda =
+	  busquedaProducto === ""
+		? filtroProductos
+		: busquedaResultado.length > 0
+		? busquedaResultado
+		: [];
+  
+	useEffect(() => {
+	  // Filtrar los productos por nombre si hay un término de búsqueda
+	  if (busquedaProducto !== "") {
+		const filteredResults = filtroProductos.filter((item) =>
+		  item.nombre.toLowerCase().includes(busquedaProducto.toLowerCase())
+		);
+		setbusquedaResultado(filteredResults);
+	  }
+	}, [busquedaProducto, filtroProductos]);
 
 	return (
 		<div className="min-vh-100">
-			<div className="container-fluid bg-secondary-subtle p-5 bg-white bg-opacity-50">
-				<h4 className="title-catalogo pb-2">Belleza simplificadad.</h4>
-				<h4 className="title-catalogo pb-2">Solo con el mejor producto natural en cada categoria de belleza.</h4>
-				<h4 className="title-catalogo pb-2">Todo lo que necessitas y nada que no.</h4>
-				<ul className="list-group list-group-horizontal p-2">
+			<div className="container-fluid bg-secondary-subtle p-5 bg-white bg-opacity-75">
+				<h4 className="title-catalogo pb-2  d-flex justify-content-center">Belleza simplificada</h4>
+				<h4 className="title-catalogo pb-2  d-flex justify-content-center">Solo con el mejor producto natural en cada categoría de belleza</h4>
+				<h4 className="title-catalogo pb-2  d-flex justify-content-center">Todo lo que necesitas</h4>
+				<div className="d-flex justify-content-center">
+				<ul className="bg-dark list-group list-group-horizontal p-2 d-inline-flex justify-content-center">
 					<li className="list-group-item bg-dark text-white" onClick={e => setFilter("")}>Todos</li>
-					{store.tipo_producto?.map(item => <li className="list-group-item bg-dark text-white" key={item.id_tipo} id={item.id_tipo} onClick={e => setFilter(item.id_tipo)}>{item.nombre}</li>)}</ul>
-			</div>
-
-			<div className="row justify-content-center">
-				{store.productos?.filter(item => filter === "" ? store.productos : item.id_tipo === filter).map(item => (
-					<ProductoCatalogo
-						key={item.id_producto}
-						producto={item}
-					/>
-				))}
-			</div>
-
+					{store.tipo_producto?.map(item => <li className="list-group-item bg-dark text-white" key={item.id_tipo} id={item.id_tipo} onClick={e => setFilter(item.id_tipo)}>{item.nombre}</li>)}
+					
+					<form className="d-inline-flex justify-content-center ms-2" role="search">
+						<input
+							className="form-control me-2 w-auto "
+							type="search"
+							placeholder="Busca tu producto"
+							aria-label="Search"
+							value={busquedaProducto}
+							onChange={(e) => setbusquedaProducto(e.target.value)}
+						></input>
+						<button
+							className="btn btn-outline-light bg-dark border-0"
+							type="submit"
+						>
+							<i className="fa-solid fa-magnifying-glass"></i>
+						</button>
+					</form>
+				
+					</ul>
 		</div>
-	);
-};
+      </div>
 
+      <div className="row justify-content-center">
+        {filtroPorBusqueda.map((item) => (
+          <ProductoCatalogo key={item.id_producto} producto={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
